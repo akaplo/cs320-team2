fc192d2a9b690c7b93b96cbaa76f1f7ff1d1cd69
Walter Brown <zinecarton@gmail.com>
2016-04-11 19:15:45 -0400

    Added outline of preferences tab.

--- src/js/controllers/controllers.js
+++ src/js/controllers/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+var module = angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
---
+++ src/js/controllers/preferences.js
@@ -0,0 +1,4 @@
+//module.controller('PreferencesCtrl', function($scope) {
+//  $scope.splashScreen = "https://s3.amazonaws.com/codecademy-blog/assets/puppy-main_zps26d178c5.jpg";
+//})
+
