2a95996ff146d40653e882bccf3f4bbd148e4fab
Raymond <rmtang@umass.edu>
2016-05-02 16:19:26 -0400

    Edited getSpecificStategies function

--- src/js/services/GetHelpService.js
+++ src/js/services/GetHelpService.js
@@ -1,33 +1,13 @@
 app.factory('GetHelp', function(sqlService) {
 
-	/* Proposed IDs associated with moods
-	Mood        ID
-	Anger		0
-    Disgust		1
-    Fear		2
-    Happiness	3
-    Sadness		4
-    Surprise	5
-	*/
-
-	/*
-	//Proposed constructor for feedback objects
-	function feedback(copingStrategy, response) {
-		//response === 1 if user responded 'yes' to the copingStrategy
-		//response === 0 if user responded 'no' to the copingStrategy
-		this.copingStrategy = copingStrategy;
-		this.response = response;
-	}
-	*/
-
-	var latestMoodLog = {
-		mood: 5
-	};
-//Get all the mood logs?
+	//The user's latest logged mood
+	var latestMood;
+	
+	//Get all the mood logs
 	sqlService.executeQuery('SELECT * FROM mood_logs').then(function(result){
 		//Get the latest mood log. Latest mood log is at the (length - 1)th row
 		console.log("Query result", result.rows.item(result.rows.length - 1))
-		latestMoodLog.mood = result.rows.item(result.rows.length - 1).id;
+		latestMood = result.rows.item(result.rows.length - 1).mood;
 	}),
 		(err) => console.log("Query error", err)
 
@@ -78,33 +58,26 @@ app.factory('GetHelp', function(sqlService) {
 	//Returns an array of coping strategies for the user's latest mood
 	function getSpecificStrategies() {
 		//Select coping strategies that match the latest mood
-		switch(latestMoodLog.mood) {
-			//If mood is angry
-			case 0:
-				return angerStrats;
-			break;
-			//If mood is disgust
-			case 1:
-				return disgustStrats;
-			break;
-			//If mood is fear
-			case 2:
-				return fearStrats;
-			break;
-			//If mood is happiness
-			case 3:
-				return happinessStrats;
-			break;
-			//If mood is sadness
-			case 4:
-				return sadnessStrats;
-			break;
-			//If mood is surprise
-			case 5:
-				return surpriseStrats;
-			break;
-			default:
-				return generalStrats;
+		if(latestMood === "angry") {
+			return angerStrats;
+		}
+		else if(latestMood === "disgust") {
+			return disgustStrats;
+		}
+		else if(latestMood === "fear") {
+			return fearStrats;
+		}
+		else if(latestMood === "happiness") {
+			return happinessStrats;
+		}
+		else if(latestMood === "sadness") {
+			return sadnessStrats;
+		}
+		else if(latestMood === "surprise") {
+			return surpriseStrats;
+		}
+		else {
+			return generalStrats;
 		}
 	}
 
